90796abd9a821b92429eea50052933f0d3176982
NJW <shwodn123@gmail.com>
2025-12-14 02:58:04 +0900

    feat : 몬스터 특정 좌표로 이동하는 움직임 구현

---
+++ Assets/Scripts/Monster/MonsterMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class MonsterMovement : MonoBehaviour
+{
+    [SerializeField] private Transform moveTransform;
+
+    private NavMeshAgent navMeshAgent;
+    private PlayerInput monsterInput;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void OnDestroy()
+    {
+        monsterInput.actions["Move"].started -= OnMove;
+    }
+
+    private void Init()
+    {
+        // 내비매쉬 에이전트 겟 컴포넌트 후 정지 상태로 변경
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.isStopped = true;
+
+        // 인풋 시스템 설정
+        monsterInput = GetComponent<PlayerInput>();
+        monsterInput.actions["Move"].started += OnMove;
+    }
+    // 입력받은 위치로 이동하는 매서드
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log($"{name} {moveTransform.name}로 이동 시작 ");
+        navMeshAgent.SetDestination(moveTransform.position);
+        navMeshAgent.isStopped = false;
+    }
+}
