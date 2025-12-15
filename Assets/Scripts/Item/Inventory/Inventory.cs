89f7be6e79733ea7ed35a7fe41de466b0f1b1ce8
ForestPool <zxpqzx123456@gmail.com>
2025-12-15 14:20:13 +0900

    feat : 인벤토리와 창고 기능의 매니저 분리
    변경 사항
    - 매니저로 만들었는데 아이템을 배열로 들고 있는 스크립트와 그걸 관리해줄 매니저로 분리하였습니다
    
    - 각각 알아보기 편하게 폴더를 나눴습니다

---
+++ Assets/Scripts/Item/Inventory/Inventory.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public ItemTableData[] slots;
+
+    private int inventorySize = 4;
+
+
+    private void Start()
+    {
+        SetInventory(inventorySize);
+    }
+
+    public void SetInventory(int size)
+    {
+        if (size < 1)
+        {
+            Debug.Log("크기가 최소 1 이상이여야 함");
+            return;
+        }
+            
+        slots = new ItemTableData[size];
+    }
+
+    public bool CheckEmpty(out int index)
+    {
+        index = -1;
+
+        if (slots == null)
+        {
+            Debug.Log("인벤토리 자체가 없습니다");
+            return false;
+        }
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i] != null)
+                continue;
+
+            // 인벤토리가 비어있을 시
+
+            index = i;
+            return true;
+        }
+
+        return false;
+    }
+
+    public void GetItem(ItemTableData item, int index)
+    {
+        if (slots == null)
+        {
+            Debug.Log("인벤토리 자체가 없습니다");
+            return;
+        }
+
+        if (item == null)
+        {
+            Debug.Log("아이템이 없습니다");
+            return;
+        }
+
+        if (index < 0 || index >= slots.Length)
+            return;
+
+        if (slots[index] != null)
+        {
+            Debug.Log("이미 아이템이 들어있습니다");
+            return;
+        }
+             
+        slots[index] = item;
+        return;
+    }
+
+    // 아이템 이동(버리거나 창고)
+    public ItemTableData MoveItem(int index)
+    {
+        if (slots == null)
+            return null;
+
+        if (index < 0 || index >= slots.Length)
+            return null;
+
+        if (slots[index] == null)
+            return null;
+
+        ItemTableData item = slots[index];
+
+        RemoveItem(index);
+
+        return item;
+    }
+
+    // 배열에서 아이템 제거
+    private void RemoveItem(int index)
+    {
+        if (slots == null)
+            return;
+
+        if (index < 0 || index >= slots.Length)
+            return;
+
+        if (slots[index] == null)
+            return;
+
+        slots[index] = null;
+    }
+
+
+}
---
+++ Assets/Scripts/Item/Manager/StorageManager.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class StorageManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private Storage storage;
+    private Inventory inventory;
+
+    public void GetStorage()
+    {
+        if (player == null)
+            return;
+
+        storage = player.GetComponentInChildren<Storage>();
+    }
+    public void GetInventory()
+    {
+        if (player == null)
+            return;
+
+        inventory = player.GetComponentInChildren<Inventory>();
+    }
+
+    // 인벤토리에서 아이템 가져오기
+    public void InventoryToStorage(ItemTableData item)
+    {
+        if (storage == null) return;
+
+        if (inventory == null) return;
+
+        int storageIndex = -1;
+        bool empty = storage.CheckEmptyStorage(out storageIndex);
+
+        if (empty == false)
+        {
+            Debug.Log("창고가 꽉 찾을시 원하는 코드");
+            return;
+        }
+
+        storage.ItemStorage(item, storageIndex);
+    }
+
+    // 인벤토리에 아이템 보내기
+    public void StorageToInventory(int index)
+    {
+        if (storage == null) return;
+
+        if (inventory == null) return;
+
+        int inventoryIndex = -1;
+
+        bool empty = inventory.CheckEmpty(out inventoryIndex);
+
+        if (empty == false)
+        {
+            Debug.Log("창고에서 인벤토리로 보낼때 꽉 찾을 시 코드");
+            return;
+        }
+
+        ItemTableData item = storage.MoveItem(index);
+
+        if (item == null) return;
+
+        inventory.GetItem(item,inventoryIndex);
+    }
+
+    public void SelectItem()
+    {
+
+    }
+}
