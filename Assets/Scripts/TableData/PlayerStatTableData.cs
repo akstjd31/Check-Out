41693b5bba35a9650e1883e18e241fe5a91d1512
Manseong Han <akstjd31@naver.com>
2025-12-15 10:53:17 +0900

    feat : PlayerStatTableData.cs 생성
    테이블에서 불러올 각 데이터들을 담을 테이블 데이터 생성

---
+++ Assets/Scripts/TableData/PlayerStatTableData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerStatTableData : TableBase
+{
+    public int id { get; set; }
+    public string configName { get; set; }
+    public string configValue { get; set; }
+    
+}
