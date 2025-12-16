620f2ac490b6ea4155e023bf5aaadb4b21624065
Manseong Han <akstjd31@naver.com>
2025-12-16 12:20:37 +0900

    feat : FadeController.cs 생성
    페이드 인 관련 스크립트 작성

---
+++ Assets/Scripts/UI/FadeController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeController : Singleton<FadeController>
+{
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeDuration = 1f;
+
+    private void Start()
+    {
+        StartFadeIn();
+    }
+
+    public void StartFadeIn() => StartCoroutine(FadeIn());
+
+    private IEnumerator FadeIn()
+    {
+        float time = fadeDuration;
+        while (time > 0f)
+        {
+            time -= Time.deltaTime;
+            SetAlpha(time / fadeDuration);
+            yield return null;
+        }
+
+        SetAlpha(0f);
+    }
+
+    private void SetAlpha(float alpha)
+    {
+        Color c = fadeImage.color;
+        c.a = Mathf.Clamp01(alpha);
+        fadeImage.color = c;
+    }
+}
