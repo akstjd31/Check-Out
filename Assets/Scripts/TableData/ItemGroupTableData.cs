18161e99f69f78a6593c4a78dff867567eaab86a
Manseong Han <akstjd31@naver.com>
2025-12-17 18:23:07 +0900

    chore : 불필요한 클래스 삭제

--- Assets/Scripts/TableData/ItemGroupTableData.cs
+++ Assets/Scripts/TableData/ItemGroupTableData.cs
@@ -1,7 +1,5 @@
-﻿using UnityEngine;
-public class ItemGroupTableData : TableBase
+﻿public class ItemGroupTableData : TableBase
 {
-    [SerializeField] ItemTableData itemTable;
     public int id { get; set; }
     public int itemGroup { get; set; }
     public int itemId { get; set; }
