c7969ad7df0b2dffa84bdeb535e04c0f03d8d5d6
ForestPool <zxpqzx123456@gmail.com>
2025-12-17 12:29:15 +0900

    test : 테스트를 위한 프리팹 생성
    Chest와 Vending Machine배치

---
+++ Assets/Scripts/InteractableObject/Chest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Chest : Interactable
+{
+    public override void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnFocusEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnFocusExit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
