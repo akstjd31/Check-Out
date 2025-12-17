5a4ce5ac63c39ab13f9a0be80ca8d6d2aba7097d
Manseong Han <akstjd31@naver.com>
2025-12-17 11:56:05 +0900

    chore : 씬에 필요한 캔버스 추가

--- Assets/Scripts/InteractableObject/Elevator.cs
+++ Assets/Scripts/InteractableObject/Elevator.cs
@@ -4,14 +4,16 @@ public class Elevator : Interactable
 {
     public override void OnFocusEnter()
     {
+        promptText = "Press [E] to Use Elevator";
     }
 
     public override void OnFocusExit()
     {
-        
+        promptText = "";
     }
 
     public override void Interact()
     {
+        GameManager.Instance.ChangeState(GameState.Loading);
     }
 }
