d18d73bc53b392d125e6d65b19de3a38bee9c868
Manseong Han <akstjd31@naver.com>
2025-12-17 10:24:38 +0900

    feat : Elevator.cs 생성
    상호작용을 위한 엘리베이터 스크립트

---
+++ Assets/Scripts/InteractableObject/Elevator.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Elevator : Interactable
+{
+    public override void OnFocusEnter()
+    {
+    }
+
+    public override void OnFocusExit()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+    }
+}
