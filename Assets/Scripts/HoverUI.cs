fcd94cef552248d7e78858b882855462c3db8641
ForestPool <zxpqzx123456@gmail.com>
2025-12-18 18:58:48 +0900

    docs : 주석 추가
    기타 수정사항
    - 불필요한 코드 삭제
    - 불필요한 기능 삭제

--- Assets/Scripts/HoverUI.cs
+++ Assets/Scripts/HoverUI.cs
@@ -12,21 +12,29 @@ public class HoverUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI priceText;
     [SerializeField] private Image itemImage;
 
+    private string firstPriceText;
+
+    private void Start()
+    {
+        firstPriceText = priceText.text;
+    }
+
+    // 마우스가 위로 올라가져 있을때
     public void OnEnter(Transform pos, ItemTableData data, Sprite sprite)
     {
         gameObject.SetActive(true);
         nameText.text = data.itemName;
         typeText.text = data.itemType;
         descText.text = data.itemDescription;
-        string changePrice = priceText.text;
-        changePrice = changePrice.Replace("{price}", data.sellPrice.ToString());
-        priceText.text = changePrice;
+        priceText.text = firstPriceText;
+        priceText.text = priceText.text.Replace("{price}", data.sellPrice.ToString());
         itemImage.sprite = sprite;
         nameText.text = data.itemName;
         
         transform.position = pos.position;
     }
 
+    // 마우스가 빠져나갔을 때
     public void OnExit()
     {
         gameObject.SetActive(false);
