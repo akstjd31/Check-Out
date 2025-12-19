d3344ac0a7d21719e5a613a741f1d70cebd6a0ea
Manseong Han <akstjd31@naver.com>
2025-12-19 16:41:15 +0900

    docs : 몬스터 스폰 테이블 추가

---
+++ Assets/Scripts/TableData/MonsterSpawnTableData.cs
@@ -0,0 +1,9 @@
+﻿public class MonsterSpawnTableData : TableBase
+{
+    public int id { get; set; }
+    public float locationX { get; set; }
+    public float locationY { get; set; }
+    public float locationZ { get; set; }
+    public bool isRandom { get; set; }
+    public string monsterId { get; set; }
+}
