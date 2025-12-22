4f6463044facc610aa472ab6df2e99562665eb0b
ForestPool <zxpqzx123456@gmail.com>
2025-12-22 10:03:05 +0900

    feat : 몬스터 컨트롤러 분리

---
+++ Assets/Scripts/Monster/MonsterController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    private void StartPatrol()
+    {
+    }
+
+    private void Find()
+    {
+    }
+    private void StartAlert()
+    {
+    }
+
+    public void StateChange(Monster.MonsterState state)
+    {
+    }
+}
