fd5656a011fb7b63c9378603b3f507c1f809582a
NJW <shwodn123@gmail.com>
2025-12-22 19:28:55 +0900

    feat : echo 구현

--- Assets/Scripts/Monster/Echo/EchoController.cs
+++ Assets/Scripts/Monster/Echo/EchoController.cs
@@ -1,6 +1,182 @@
+using System.Collections;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class EchoController : MonsterController
 {
-    
+    [SerializeField] private EchoView echoView;
+    private EchoModel echoModel;
+<<<<<<< Updated upstream
+    private EchoController echoController;
+    private FieldOfView echoFieldOfView;
+
+    // 플레이어를 저장할 변수
+    private GameObject playerTransform;
+    private PlayerStateMachine playerStateMachine;
+    // 플레이어와의 거리를 저장할 변수
+    private float distanceToTarget;
+    // 플레이어를 강제로 어둠 상태로 전환하는 시간을 담을 변수
+    private WaitForSeconds holdingTime;
+    private WaitForSeconds spawnTime;
+    // PlayerSituation를 임시로 저장할 변수
+    private PlayerSituation tempPlayerSituation;
+    private void Awake()
+    {
+        //컴포넌트 추가
+        echoModel = GetComponent<EchoModel>();
+        echoView = GetComponent<EchoView>();
+        echoController = GetComponent<EchoController>();
+        echoFieldOfView = GetComponent<FieldOfView>();
+        playerStateMachine = GetComponent<PlayerStateMachine>();
+=======
+    private FieldOfView echoFieldOfView;
+    private Vector3 directionToTarget;
+
+    private void Awake()
+    {
+        // 컴포넌트 추가
+        echoModel = GetComponent<EchoModel>();
+        echoView = GetComponent<EchoView>();
+        echoFieldOfView = GetComponent<FieldOfView>();
+>>>>>>> Stashed changes
+    }
+
+    private void OnEnable()
+    {
+<<<<<<< Updated upstream
+        echoModel.OnObserve += StartObservePlayer;
+        echoModel.OnEyeContact += StartEyeContactPlayer;
+        echoModel.OnContactPlayer += StartContactPlayer;
+=======
+        //구독 설정
+        echoModel.OnObserve += StartObservePlayer;
+>>>>>>> Stashed changes
+    }
+
+    private void OnDisable()
+    {
+<<<<<<< Updated upstream
+        echoModel.OnObserve -= StartObservePlayer;
+        echoModel.OnEyeContact -= StartEyeContactPlayer;
+        echoModel.OnContactPlayer -= StartContactPlayer;
+=======
+        // 구독 해제
+        echoModel.OnObserve -= StartObservePlayer;
+>>>>>>> Stashed changes
+    }
+
+    private void Start()
+    {
+        Init();
+<<<<<<< Updated upstream
+=======
+        echoModel.ChangeState(Monster.MonsterState.Observe);
+>>>>>>> Stashed changes
+    }
+
+    private void Init()
+    {
+<<<<<<< Updated upstream
+        // 모델 값을 해당 컴포넌트에 전달
+        holdingTime = new WaitForSeconds(echoModel.DarkSituationHoldingTime);
+        spawnTime = new WaitForSeconds(echoModel.RespawnTime);
+        // Field Of View
+        echoFieldOfView.viewRadius = echoModel.ViewRadius;
+        echoFieldOfView.viewAngle = echoModel.ViewAngle;
+        echoFieldOfView.delay = new WaitForSeconds(echoModel.Delay);
+        echoFieldOfView.targetMask = echoModel.PlayerMask;
+        echoFieldOfView.obstacleMask = echoModel.ObstacleMask;
+
+        // 플레이어한테 보이는 지에 대한 변수 초기화
+        echoModel.isObservedFromPlayer = false;
+
+        // 오브젝트 비활성화
+        gameObject.SetActive(false);
+=======
+        // FieldOfView
+        echoFieldOfView.viewRadius = echoModel.ViewRadius;
+        echoFieldOfView.viewAngle = echoModel.ViewAngle;
+        echoFieldOfView.delay = new WaitForSeconds(echoModel.Delay);
+
+        // 플레이어한테 보이는 지에 대한 변수 초기화
+        echoModel.isObservedFromPlayer = false;
+>>>>>>> Stashed changes
+    }
+
+    private void StartObservePlayer()
+    {
+<<<<<<< Updated upstream
+        StartCoroutine(ObservePlayer());
+    }
+    private IEnumerator ObservePlayer()
+    {
+        while (true)
+        {
+            // 플레이어가 시야 안에 있다면 아래 내용 수행
+            if(echoFieldOfView.visibleTargets.Count > 0)
+            {
+                distanceToTarget = Vector3.Distance(echoFieldOfView.visibleTargets[0].position,transform.position);
+                // 플레이어와 에코가 일정 거리 만큼 가까우면 눈 마주침 상태로 전환
+                if (distanceToTarget < echoModel.RangeOfRecognition)
+                {
+                    // 에코 상태 관찰로 전환 ( 스폰 구현되면 스폰 후 상태 전환할 예정 )
+                    echoModel.ChangeState(Monster.MonsterState.Observe);
+                }
+            }
+
+            yield return echoFieldOfView.delay;
+        }
+    }
+
+    private void StartEyeContactPlayer()
+    {
+        StartCoroutine(EyeContactPlayer());
+    }
+
+    private IEnumerator EyeContactPlayer()
+    {
+        // 플레이어를 어둠 상황으로 변환 10초 후 관찰 상태로 변환
+        tempPlayerSituation = playerStateMachine.CurrentSituation;
+        playerStateMachine.ChangeSituation(PlayerSituation.Dark);
+        yield return holdingTime;
+        // 이후 다시 원래 상태로 돌아감
+        playerStateMachine.ChangeSituation(tempPlayerSituation);
+        
+    }
+
+    private void StartContactPlayer()
+    {
+        StartCoroutine(ContactPlayer());
+    }
+
+    private IEnumerator ContactPlayer()
+    {
+        // 에코 비활성화 후 다시 활성화
+        gameObject.SetActive(true);
+        yield return spawnTime;
+
+        // 다시 관찰 상태로 전환 ( 스폰 구현되면 스폰 후 상태 전환할 예정 )
+        echoModel.ChangeState(Monster.MonsterState.Observe);
+    }
+=======
+        ObservePlayer();
+    }
+
+    private IEnumerator ObservePlayer()
+    {
+        while(true)
+        {
+            if(echoFieldOfView.visibleTargets.Count > 0)
+            {
+                // 플레이어가 시야에 있으면 플레이어를 바라봄
+                transform.LookAt(echoFieldOfView.visibleTargets[0]);
+
+                // 플레이어가 지정 범위 내에 있으면 상태 전환
+                directionToTarget = (echoFieldOfView.visibleTargets[0].position - transform.position).normalized;
+
+            }
+            yield return echoFieldOfView.delay;
+        }
+    }
+>>>>>>> Stashed changes
 }
--- Assets/Scripts/Monster/Echo/EchoModel.cs
+++ Assets/Scripts/Monster/Echo/EchoModel.cs
@@ -1,6 +1,80 @@
+using System;
 using UnityEngine;
 
 public class EchoModel : Monster
 {
-    
+    [Header("IndividualProperties")]
+<<<<<<< Updated upstream
+    [SerializeField] private float rangeOfRecognition;
+    [SerializeField] private float darkSituationHoldingTime = 10f;
+    [SerializeField] private float respawnTime = 30f;
+=======
+    // 눈 마주침 상호작용 거리 
+    [SerializeField] private float rangeOfRecognition = 5.0f;
+>>>>>>> Stashed changes
+
+    [Header("MonsterFieldOfView")]
+    // 시야 범위 조정
+    [SerializeField] private float viewRadius;
+    // 각도 360으로 제한
+    [Range(0, 360)]
+    [SerializeField] private float viewAngle;
+    // 필요한 레이어
+    [SerializeField] private LayerMask playerMask;
+    [SerializeField] private LayerMask obstacleMask;
+    // 시야 갱신 시점 조정
+    [SerializeField] private float delay;
+
+    // 프로퍼티
+<<<<<<< Updated upstream
+    public float RangeOfRecognition { get { return rangeOfRecognition; } }
+    public float DarkSituationHoldingTime { get { return darkSituationHoldingTime; } }
+    public float RespawnTime { get { return respawnTime; } }
+=======
+>>>>>>> Stashed changes
+    public float ViewRadius { get { return viewRadius; } }
+    public float ViewAngle { get { return viewAngle; } }
+    public LayerMask PlayerMask { get { return playerMask; } }
+    public LayerMask ObstacleMask { get { return obstacleMask; } }
+    public float Delay { get { return delay; } }
+<<<<<<< Updated upstream
+
+    // 액션
+=======
+    public float RangeOfRecognition { get { return rangeOfRecognition; } }
+
+    // 이벤트 생성
+>>>>>>> Stashed changes
+    public event Action OnObserve;
+    public event Action OnEyeContact;
+    public event Action OnContactPlayer;
+
+    public override void ChangeState(MonsterState inputState)
+    {
+<<<<<<< Updated upstream
+        switch(inputState)
+=======
+        switch (inputState)
+>>>>>>> Stashed changes
+        {
+            case MonsterState.Observe:
+                monsterState = MonsterState.Observe;
+                OnObserve?.Invoke();
+                break;
+            case MonsterState.EyeContact:
+                monsterState = MonsterState.EyeContact;
+                OnEyeContact?.Invoke();
+                break;
+<<<<<<< Updated upstream
+            case MonsterState.ContactPlayer:
+                monsterState = MonsterState.ContactPlayer;
+=======
+            case MonsterState.contactPlayer:
+                monsterState = MonsterState.contactPlayer;
+>>>>>>> Stashed changes
+                OnContactPlayer?.Invoke();
+                break;
+        }
+    }
+
 }
