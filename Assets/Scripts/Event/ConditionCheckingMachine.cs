ddc4bbea33f741bf71ef901a385d64520044d5e6
Manseong Han <akstjd31@naver.com>
2025-12-23 01:17:43 +0900

    feat : EventParam.cs 생성
    - IDType 판별 및 열거형 정의

---
+++ Assets/Scripts/Event/ConditionCheckingMachine.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public static class ConditionCheckingMachine
+{
+    // public static bool Check(EventTableData data)
+    // {
+        
+    // }
+
+    // static bool CheckOne(string type, string value)
+    // {
+    //     // type이 none이면 value값도 none임 (자동 패스)
+    //     if (type == "none")
+    //         return true;
+
+    //     EventParam param = EventParam.Parse(value);
+
+    //     // 일반 수치인 경우
+    //     if (param.type.Equals(IDType.None))
+    //     {
+            
+    //     }
+    //     else
+    //     {
+    //         switch (param.type)
+    //         {
+    //             case IDType.Item:
+    //                 return 
+    //         }
+    //     }
+    // }
+}
--- Assets/Scripts/Manager/EventManager.cs
+++ Assets/Scripts/Manager/EventManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 /// <summary>
@@ -6,15 +7,23 @@ using UnityEngine;
 /// </summary>
 public class EventManager : Singleton<EventManager>
 {
-    public Dictionary<int, EventTableData> eventTable;  // <ID, 이벤트 테이블 데이터>
-    private Dictionary<string, IEventHandler> handlers; // 이벤트 핸들러
+    public Dictionary<int, List<EventTableData>> eventGroups;  // <ID, 이벤트 테이블 데이터>
 
-    public void ExecuteEvent(int eventId)
+    public void ExecuteByStart(string startType, string startValue)
     {
-        // 해당 ID에 맞는 데이터가 존재하지 않을 떄
-        if (!eventTable.TryGetValue(eventId, out var eventData))
-            return;
-        
-
+        foreach (var group in eventGroups.Values)
+        {
+            
+        }
     }
+
+    private void ExecuteGroup(List<EventTableData> group)
+    {
+        foreach (var evt in group)
+        {
+            
+        }
+    }
+
+    
 }
