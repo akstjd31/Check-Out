9c3c402786cf580c843ef1d4c56ff35c1e3accae
Manseong Han <akstjd31@naver.com>
2025-12-22 17:49:39 +0900

    feat : EventTableData.cs 생성
    - 테이블데이터 값이 들어갈 임시 데이터

--- Assets/Scripts/Manager/EventManager.cs
+++ Assets/Scripts/Manager/EventManager.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 이벤트 관련 제어 클래스
+/// </summary>
 public class EventManager : Singleton<EventManager>
 {
-
+    public Dictionary<int, EventTableData> eventTable;  // <ID, 이벤트 테이블 데이터>
+    
 }
---
+++ Assets/Scripts/TableData/EventTableData.cs
@@ -0,0 +1,5 @@
+// 이벤트 테이블 임시 데이터
+public class EventTableData : TableBase
+{
+    
+}
