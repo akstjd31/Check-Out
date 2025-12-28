72e2c7b3ae8e99398e3108c2b0cefde27e3275f8
Brew-D <bloodinbrewd@gmail.com>
2025-12-29 01:15:22 +0900

    feat : 대사창 관련 이벤트 기능 추가
    이벤트 팩토리 내에 대사창 관련 기능이 없던 문제 해결, 이벤트 테이블에 테스트용 대화창 출력 이벤트 추가

--- Assets/Scripts/Event/EventAction/EventFactory.cs
+++ Assets/Scripts/Event/EventAction/EventFactory.cs
@@ -1,4 +1,3 @@
-using UnityEngine;
 public static class EventFactory
 {
     public static IEventAction Create(string eventType)
@@ -8,7 +7,6 @@ public static class EventFactory
             case "setSwitchOn":
                 return new SetSwitchOnAction();
             case "soundOnce":
-                Debug.Log("소리 재생 이벤트 발동");
                 return new SoundOnceAction();
             case "showText":
                 return new ShowTextAction();
