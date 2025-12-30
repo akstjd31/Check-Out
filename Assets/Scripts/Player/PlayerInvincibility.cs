9a7c0c57c9406a6476a088ec329cd1fa8a8b2023
Manseong Han <akstjd31@naver.com>
2025-12-31 03:26:19 +0900

    fixed : 플레이어 무적 부분 수정

--- Assets/Scripts/Player/PlayerInvincibility.cs
+++ Assets/Scripts/Player/PlayerInvincibility.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerSanity))]
@@ -8,94 +6,85 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerCameraController))]
 public class PlayerInvincibility : MonoBehaviour
 {
-
     public Monster hitMonster { get; set; }
+
     [Header("Component")]
     private StatController stat;
     private PlayerCameraController playerCamera;
     private PlayerSanityVisualController visual;
     private PlayerSoundController soundController;
     private PlayerStateMachine stateMachine;
+    private EchoSpawnSystem echoSpawnSystem;
+
+    [Header("Invincibility")]
     private float invincibleTimer;
     private float invincibleTime;
 
-    [Header("Value")]
-    [SerializeField] private bool isInvincible = false;  // 무적 상태인지?
+    [SerializeField] private bool isInvincible = false;
     public bool onHit = false;
-
-    private EchoSpawnSystem echoSpawnSystem;
 
     private void Awake()
     {
-        stat = this.GetComponent<StatController>();
-        playerCamera = this.GetComponent<PlayerCameraController>();
-        visual = this.GetComponent<PlayerSanityVisualController>();
-        soundController = this.GetComponent<PlayerSoundController>();
-        echoSpawnSystem = this.GetComponent<EchoSpawnSystem>();
-        stateMachine = this.GetComponent<PlayerStateMachine>();
-
-        invincibleTime = stat.DefaultInvincibilityTime;
+        stat = GetComponent<StatController>();
+        playerCamera = GetComponent<PlayerCameraController>();
+        visual = GetComponent<PlayerSanityVisualController>();
+        soundController = GetComponent<PlayerSoundController>();
+        echoSpawnSystem = GetComponent<EchoSpawnSystem>();
+        stateMachine = GetComponent<PlayerStateMachine>();
     }
+
+    private void Start() => invincibleTime = stat.DefaultInvincibilityTime;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("DamagedArea"))
-        {
-            if (isInvincible)
-                return;
-            Monster model = other.GetComponentInParent<Monster>();
-            if (model == null)
-                return;
-
-            if (model != null)
-            {
-                hitMonster = model;
-            }
-
-            hitMonster = model;
-
-            if (stat.IsRemainSanity())
-            {
-                if (model is MannequinModel)
-                    SoundManager.Instance.PlayMannequinAttackSound();
-                else if (model is SirenModel || model is WalkerModel)
-                    SoundManager.Instance.PlayWalkerAndSirenAttackSound();
-
-                if (model is EchoModel)
-                {
-                    SoundManager.Instance.PlayEchoLaughSound();
-                    echoSpawnSystem.DisableEcho();
-                }
-            }
-        }
-    }
-
-    private void UpdateMonsterDamage()
-    {
+        if (!other.CompareTag("DamagedArea"))
+            return;
 
         if (isInvincible)
             return;
 
-        if (hitMonster == null)
+        Monster monster = other.GetComponentInParent<Monster>();
+        if (monster == null)
             return;
 
-        if (stat.IsRemainSanity())
-        {
-            onHit = true;
-            playerCamera.Hit();
-            Debug.LogWarning("데미지 입음!");
-
-            stat.ChangeSanity(onHit, -hitMonster.Power);
-            Debug.Log(onHit);
-            OnHitInvincible();
-
-            visual.UpdateShake(onHit);
-            onHit = false;
-        }
-        
+        ApplyDamage(monster);
     }
 
-    private void OnHitInvincible()
+    private void ApplyDamage(Monster monster)
+    {
+        if (!stat.IsRemainSanity())
+            return;
+
+        onHit = true;
+        hitMonster = monster;
+
+        // 🔊 사운드 처리
+        if (monster is MannequinModel)
+            SoundManager.Instance.PlayMannequinAttackSound();
+        else if (monster is SirenModel || monster is WalkerModel)
+            SoundManager.Instance.PlayWalkerAndSirenAttackSound();
+        else if (monster is EchoModel)
+        {
+            SoundManager.Instance.PlayEchoLaughSound();
+            echoSpawnSystem.DisableEcho();
+        }
+
+        // 📷 카메라 & 비주얼
+        playerCamera.Hit();
+        visual.UpdateShake(onHit);
+
+        // 💥 데미지 적용 (1회)
+        stat.ChangeSanity(onHit, -monster.Power);
+        Debug.LogWarning("플레이어 데미지 1회 적용");
+
+        // 🛡 무적 시작
+        StartInvincibility();
+
+        onHit = false;
+        hitMonster = null; // 중복 데미지 방지
+    }
+
+    private void StartInvincibility()
     {
         isInvincible = true;
         invincibleTimer = invincibleTime;
@@ -113,9 +102,9 @@ public class PlayerInvincibility : MonoBehaviour
             isInvincible = false;
         }
     }
+
     private void Update()
     {
-        UpdateMonsterDamage();
         UpdateInvincibility();
     }
 }
