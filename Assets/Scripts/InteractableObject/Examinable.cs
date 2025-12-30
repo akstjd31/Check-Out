41745a88147a20a75f9b2cb8da2fbd18e9f3d3ca
Manseong Han <akstjd31@naver.com>
2025-12-30 12:34:55 +0900

    feat : Examinable.cs 이벤트 추가

--- Assets/Scripts/InteractableObject/Examinable.cs
+++ Assets/Scripts/InteractableObject/Examinable.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 // 오브젝트 조사 관련
 public class Examinable : Interactable
 {
+    [SerializeField] private Animator anim;
+
     private void Awake()
     {
+        anim = this.GetComponent<Animator>();
+        audioSource = this.GetComponent<AudioSource>();
     }
+    
     public override void OnFocusEnter()
     {
         currentText = promptText[0];
@@ -20,5 +25,7 @@ public class Examinable : Interactable
 
     public override void Interact()
     {
+        if (eventObj != null)
+            EventManager.Instance.OnEventTriggered(eventObj.StartType, eventObj.StartValue);
     }
 }
